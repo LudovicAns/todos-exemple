935990cd3a26f888cea3ee3f6df13f3f35035781
LudovicAns <ludovicanselin@gmail.com>
2025-01-23 11:33:37 +0100

    refactor: extract registration logic to a dedicated component
    Move registration form logic from Register.jsx to a new RegisterForm.jsx component. This improves code reusability, clarity, and modularity by separating concerns between the page and form components.

--- todos-app/src/layouts/AuthLayout.jsx
+++ todos-app/src/layouts/AuthLayout.jsx
@@ -4,7 +4,7 @@ import {Outlet} from "react-router";
 function AuthLayout(props) {
     document.body.classList.add('bg-zinc-900');
     return (
-        <main className={`flex items-center justify-center mt-8`}>
+        <main className={`flex items-center justify-center mt-8 mb-8`}>
             <Outlet/>
         </main>
     );
