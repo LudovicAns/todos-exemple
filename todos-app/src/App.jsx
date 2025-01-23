2e2d67354adf34a9a55147323e0c124bc5a456db
LudovicAns <ludovicanselin@gmail.com>
2025-01-23 14:31:58 +0100

    feat: add Layout component for consistent page structure
    Introduce a new Layout component to wrap pages with a consistent structure, including a centered layout and styling. Updated Home page with links to Register and Todos using styled text components. Adjusted routing to integrate the Layout component seamlessly.

--- todos-app/src/App.jsx
+++ todos-app/src/App.jsx
@@ -4,15 +4,18 @@ import Todos from "./pages/Todos.jsx";
 import AuthLayout from "./layouts/AuthLayout.jsx";
 import Home from "./pages/Home.jsx";
 import Register from "./pages/auth/Register.jsx";
+import Layout from "./layouts/Layout.jsx";
 
 function App() {
 
     return (
         <Routes>
-            <Route index element={<Home/>}/>
+            <Route element={<Layout/>}>
+                <Route path={``} element={<Home/>}/>
+            </Route>
 
             <Route element={<AuthLayout/>}>
-                <Route path={`register`} element={<Register/>}></Route>
+                <Route path={`register`} element={<Register/>}/>
             </Route>
 
             <Route path={`todos`} element={<Todos/>}/>
--- todos-app/src/pages/Home.jsx
+++ todos-app/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import {Text, TextLink} from "../composants/catalyst-ui/text.jsx";
 
 function Home(props) {
+    document.body.classList.add('bg-zinc-900');
     return (
-        <div></div>
+        <ul>
+            <li><Text>Pour s'enregistrer c'est par <TextLink href={"/register"}>ici</TextLink></Text></li>
+            <li><Text>Et la Todo List c'est par <TextLink href={"/todos"}>ici</TextLink></Text></li>
+        </ul>
     );
 }
 
